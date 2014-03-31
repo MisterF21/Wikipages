b9ccdf3341e237e0f43cfceb60e7cabcc580ef59
Drew Seminara & Marty Fajardo <student@epicodus.com>
2014-03-31 11:59:57 -0700

    complete wikipage contact app, with CRUD ability

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe Contact do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :phone }
+
 end
